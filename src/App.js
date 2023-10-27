82e1bba62408426c3f43b6e286c889f20f7a4ee8
hector918 <hector_zhong@outlook.com>
2023-10-27 12:48:11 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FindMeetingRoom from './pages/find-meeting-room';
+/////////////////////////////////////////////////////
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FindMeetingRoom />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+/////////////////////////////////////////////////////
 
 export default App;
